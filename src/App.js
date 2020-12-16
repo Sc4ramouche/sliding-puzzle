d0e94b85a583a93941d79c13091dc62c1710b1b3
sc4ramouche <vladislav.kovechenkov@gmail.com>
2020-12-17 00:54:45 +0300

    Cutting images into separate blocks with background position

--- src/App.js
+++ src/App.js
@@ -1,23 +1,69 @@
-import logo from './logo.svg';
 import './App.css';
+
+const SIZE = 3;
+
+const board = [];
+
+for (let i = 0; i < SIZE; i++) {
+  const row = [];
+  for (let j = 0; j < SIZE; j++) {
+    row.push({ x: i, y: j });
+  }
+  board.push(row);
+}
+console.log(board);
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div style={{ width: '300px', height: '300px', backgroundColor: 'blue' }}>
+        <div className="row">
+          <div
+            className="piece background"
+            style={{ backgroundPositionX: '0%' }}
+          ></div>
+          <div
+            className="piece background"
+            style={{ backgroundPositionX: '50%' }}
+          ></div>
+          <div
+            className="piece background"
+            style={{ backgroundPositionX: '100%' }}
+          ></div>
+        </div>
+
+        <div className="row">
+          <div
+            className="piece background"
+            style={{ backgroundPositionX: '0%', backgroundPositionY: '50%' }}
+          ></div>
+          <div
+            className="piece background"
+            style={{ backgroundPositionX: '50%', backgroundPositionY: '50%' }}
+          ></div>
+          <div
+            className="piece background"
+            style={{ backgroundPositionX: '100%', backgroundPositionY: '50%' }}
+          ></div>
+        </div>
+
+        <div className="row">
+          <div
+            className="piece background"
+            style={{ backgroundPositionX: '0%', backgroundPositionY: '100%' }}
+          ></div>
+          <div
+            className="piece background"
+            style={{ backgroundPositionX: '50%', backgroundPositionY: '100%' }}
+          ></div>
+          <div
+            className="piece background"
+            style={{ backgroundPositionX: '100%', backgroundPositionY: '100%' }}
+          ></div>
+        </div>
+
+        {/* <img src="https://picsum.photos/300" /> */}
+      </div>
     </div>
   );
 }
